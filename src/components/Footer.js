7738d583022ab56c96a9b4c9ff98bca1d2ceabe7
Christine Wong <jumiryoo@gmail.com>
2023-10-29 00:53:22 -0700

    initial commit of portfolio v.1

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+import {motion} from 'framer-motion'
+
+const Footer = () => {
+    return(
+        <footer className='w-full border-t-2 border-solid border-dark
+        font-medium text-lg
+        '>
+            <Layout className='py-8 flex itemx-center justify-between'>
+                <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+                <div className='flex items-center'>Built With <span className='text-primary text-2xl px-1'>&#9825;</span>
+                    by&nbsp; <Link href="/">Christine Wong</Link>
+                </div>
+                
+                <motion.a href="mailto:jumiryoo@gmail.com" target={"_blank"}
+                 whileHover={{y:-2}}
+                 whileTap={{scale:0.9}}>Say Hello &#128075;</motion.a>
+
+            </Layout>
+        </footer>
+    )
+}
+
+export default Footer
